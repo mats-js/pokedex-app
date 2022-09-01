d34acacf830162b76d144fe2a7e7d2ed8109198a
mats-js <100519629+mats-js@users.noreply.github.com>
2022-09-01 16:15:29 +0200

    Removed a typo from the scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 let pokemonList = [
     { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
     { name: 'Charmander', height: 0.6, types: ['fire']},
-    { name: 'Squirtle', height: 0.5, types: ['water']},
+    { name: 'Squirtle', height: 0.5, types: ['water']}
 ];
