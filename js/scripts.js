83df1da4593db4ee4b63017102627599f9919cf2
mats-js <100519629+mats-js@users.noreply.github.com>
2022-09-01 16:13:37 +0200

    Added array to scripts.js and amended README.md text
    - Removed the previous content of the scripts.js file
    - Included a new "pokemonList" array in scripts.js with three objects
    - Described the project content and requirements in the README.md file

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-alert('Hello world');
-
-// Define a variable favoriteFood and display it on the document
-let favoriteFood = "Pasta";
-document.write(favoriteFood);
+let pokemonList = [
+    { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
+    { name: 'Charmander', height: 0.6, types: ['fire']},
+    { name: 'Squirtle', height: 0.5, types: ['water']},
+];
