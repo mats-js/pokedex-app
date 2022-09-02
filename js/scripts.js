8454e08b432124c9ed65e527a0b8beb62d99aa1c
mats-js <100519629+mats-js@users.noreply.github.com>
2022-09-02 12:11:28 +0200

    Amended script to print pokemon info to document
    - Included a for loop with an if-else-conditional to print pokemon names and heights to the document
    - Added some basic styling in the styles.css file
    - Corrected the index.html indentation from 2 to 4 characters to be in line with project conventions

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,19 @@
+// define a list of pokemon in the array pokemonList
 let pokemonList = [
     { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
     { name: 'Charmander', height: 0.6, types: ['fire']},
     { name: 'Squirtle', height: 0.5, types: ['water']}
 ];
+
+// define threshold for pokemon height (specifically chosen so that only one pokemon reaches the threshold)
+let heightBig = 0.7;
+
+// for loop to iterate over each item in pokemonList
+for (i = 0; i < pokemonList.length; i++) {
+    // if height of pokemon is equal to or greater than the height threshold, label pokemon as 'Wow, that's big'
+    if (pokemonList[i].height >= heightBig) {
+        document.write(`<li>${pokemonList[i].name} (height: ${pokemonList[i].height}m) - Wow, that's big!</li>`);
+    } else {
+        document.write(`<li>${pokemonList[i].name} (height: ${pokemonList[i].height}m)</li>`);
+    }
+}
