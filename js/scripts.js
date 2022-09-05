78c90ad4dd44d01f4b4272eea497bb111f14c2b0
mats-js <100519629+mats-js@users.noreply.github.com>
2022-09-05 17:11:20 +0200

    Updated scripts.js as an IIFE
    - Wrapped pokemonList in an IIFE
    - Assigned the IIFE to a newly created variable pokemonRepository
    - Returned an object with public functions getAll and add
    - Refactored the forEach() loop to be able to access the IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,34 @@
-// define a list of pokemon in the array pokemonList
-let pokemonList = [
-    { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
-    { name: 'Charmander', height: 0.6, types: ['fire']},
-    { name: 'Squirtle', height: 0.5, types: ['water']}
-];
+// create a new variable pokemonRepository and assign the IIFE to it
+let pokemonRepository = (function() {
+    // define a list of pokemon in the array pokemonList
+    let pokemonList = [
+        { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
+        { name: 'Charmander', height: 0.6, types: ['fire']},
+        { name: 'Squirtle', height: 0.5, types: ['water']}
+    ];
+
+    // define separate function getAll()
+    function getAll() {
+        return pokemonList;
+    }
+
+    // define separate function add(item)
+    function add(item) {
+        pokemonList.push(item);
+    }
+
+    // return object with the new public functions assigned as keys
+    return {
+        getAll: getAll,
+        add: add
+    }
+})()
 
 // define threshold for pokemon height (specifically chosen so that only one pokemon reaches the threshold)
 let heightBig = 0.7;
 
 // forEach loop to iterate over each pokemon in pokemonList
-pokemonList.forEach( function(pokemon) {
+pokemonRepository.getAll().forEach( function(pokemon) {
     // if height of pokemon is equal to or greater than the height threshold, label pokemon as 'Wow, that's big'
     if (pokemon.height >= heightBig) {
         document.write(`<li>${pokemon.name} (height: ${pokemon.height}m) - Wow, that's big!</li>`);
